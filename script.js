ebe41d275135529494134be87bac05b42d029d47
AGRIMA VIJAYVARGIYA <agrimavijay04@gmail.com>
2026-06-26 11:00:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const testOutput = `test_dataType1 (__main__.TestSolution.test_dataType1) ... ok
+test_dataType2 (__main__.TestSolution.test_dataType2) ... ok
+test_sanity (__main__.TestSolution.test_sanity) ... ok
+
+----------------------------------------------------------------------
+Ran 3 tests in 0.001s
+
+OK`;
+
+document.getElementById("test-output").textContent = testOutput;
